0903f713374196fc7e28fbf6a2877a10255000a2
Megan Strachan <m.e.strachan@gmail.com>
2016-06-20 18:41:33 +0100

    initial commit

---
+++ homework_answers.sql
@@ -0,0 +1,30 @@
+-- Section 1
+
+-- Question 1
+-- SQL query:
+
+SELECT name FROM users;
+-- Terminal output:
+       name       
+------------------
+ Rick Henri
+ Jay Chetty
+ Keith Douglas
+ Ashleigh Adams
+ Euan Blackledge
+ Chris Flint
+ Nico di Lillo
+ Joe Maher
+ Marie Moyles
+ Iain Stewart
+ Megan Strachan
+ Russell Williams
+ Sam Werngren
+ Natalie Simpson
+ Davide de Lerma
+ Josh Kearns
+ Renwick Drysdale
+ Brian Morrice
+(18 rows)
+
+-- Select the names of all shows that cost less than £15.
